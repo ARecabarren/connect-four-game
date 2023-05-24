bf7800cfd01298ac9ec461aa39dd0a45f4720dc3
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-05-24 09:45:52 -0400

    Set test for valid and invalid moves

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -14,4 +14,49 @@ describe Board do
             ])
         end
     end
+    describe '#make_move' do
+        context 'with a valid move' do
+          it 'places the player\'s piece in the specified column' do
+            board = Board.new
+            board.make_move(1, 'X') # Player 1 makes a move in column 1
+            expect(board.cells).to eql([
+                ['','','','','','',''],
+                ['','','','','','',''],
+                ['','','','','','',''],
+                ['','','','','','',''],
+                ['','','','','','',''],
+                ['X','','','','','','']
+            ])
+            end
+        end
+
+        context 'with an invalid move' do
+            it 'does not place the player\'s piece if the column is full' do
+                board = Board.new
+                6.times{board.make_move(1, 'X')}
+                expect(board.cells).to eql([
+                    ['X','','','','','',''],
+                    ['X','','','','','',''],
+                    ['X','','','','','',''],
+                    ['X','','','','','',''],
+                    ['X','','','','','',''],
+                    ['X','','','','','','']
+                ])
+            end
+            it 'does not place the player\'s piece if the index is invalid' do
+                board = Board.new
+                board.make_move(O, 'X')
+                expect(board.cells).to eql([
+                    ['','','','','','',''],
+                    ['','','','','','',''],
+                    ['','','','','','',''],
+                    ['','','','','','',''],
+                    ['','','','','','',''],
+                    ['','','','','','','']
+                ])
+            end
+        end
+        
+        
+    end
 end
