b4c4399b93b60b08fb80d0f01725d5aa8c4fdb98
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-05-25 16:51:51 -0400

    Adapt flow control and methods

--- lib/board.rb
+++ lib/board.rb
@@ -8,10 +8,11 @@ class Board
     end
 
     def play_game
+        set_players
         loop do
             display_board
-            column = ask_player_for_move            make_move(column, @current_player[:symbol])
-            
+            column = ask_player_for_move            
+            make_move(column, @current_player[:symbol])
             if game_over?
                 display_board
                 puts "Game Over!"
@@ -56,7 +57,7 @@ class Board
     end
 
     def ask_player_for_move
-        puts "#{@current_player}, enter the column number (1-7): "
+        puts "#{@current_player[:name]}, enter the column number (1-7): "
         gets.chomp.to_i
     end
 
@@ -64,6 +65,7 @@ class Board
         @cells.each do |row|
             puts row.join(' ')
         end
+        puts '1 2 3 4 5 6 7' 
     end
 
     def switch_player
@@ -83,26 +85,26 @@ class Board
     def check_winner
         # Horizontal check
         @cells.each do |row|
-            return true if row.join('').include?(@current_player * 4)
+            return true if row.join('').include?(@current_player[:symbol] * 4)
         end
 
         # Vertical check
         columns = @cells.transpose
         columns.each do |column|
-            return true if column.join('').include?(@current_player * 4)
+            return true if column.join('').include?(@current_player[:symbol] * 4)
         end
 
         # Diagonals left to right
         (0..2).each do |row|
             (0..3).each do |column|
-                return true if [@cells[row][column], @cells[row + 1][column + 1], @cells[row + 2][column + 2], @cells[row + 3][column + 3]].join('').include?(@current_player * 4)
+                return true if [@cells[row][column], @cells[row + 1][column + 1], @cells[row + 2][column + 2], @cells[row + 3][column + 3]].join('').include?(@current_player[:symbol] * 4)
             end
         end
 
         # Diagonals left to right
         (0..2).each do |row|
             (3..6).each do |column|
-                return true if [@cells[row][column], @cells[row + 1][column - 1], @cells[row + 2][column - 2], @cells[row + 3][column - 3]].join('').include?(@current_player * 4)
+                return true if [@cells[row][column], @cells[row + 1][column - 1], @cells[row + 2][column - 2], @cells[row + 3][column - 3]].join('').include?(@current_player[:symbol] * 4)
             end
         end
 
